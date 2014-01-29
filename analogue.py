4df230352f3b2826d1d1096b5e5f7a6608d9029e
carl <carl@qu-be.co.uk>
2014-01-29 22:46:51 +0000

    converting value to standard analogue joystick range added to analogue.py

--- analogue.py
+++ analogue.py
@@ -19,6 +19,16 @@ def ReadChannel(channel):
   data = ((adc[1]&3) << 8) + adc[2]
   return data
 
+def convertLowerToJoystickRange(value):
+  result =-1.0+(value/520.0)
+  return result
+
+def convertUpperToJoystickRange(value):
+  result=((value-520.0)/520.0)
+  return result
+
+
+
 joy_x=0
 joy_y=1
 
@@ -26,8 +36,13 @@ while True:
     joy_x_value=ReadChannel(joy_x)
     
     print ("Joy X Value:{}".format(joy_x_value))
-
-    joy_y_value=ReadChannel(joy_y)
-
-    print ("Joy Y Value:{}".format(joy_y_value))
+   
+    if joy_x_value<515:
+     
+      print("x converted value {}".format(convertLowerToJoystickRange(joy_x_value)))
+    elif joy_x_value>525:
+      
+      print("x converted value {}".format(convertUpperToJoystickRange(joy_x_value)))
+    else:
+      print("x converted value 0")
     
